6be8184498ec999e67e473d96514c2a373df242d
Callum Melia <callum.melia@open-money.co.uk>
2022-12-01 11:00:47 +0000

    day 1 baby

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,23 +1,58 @@
 import Puzzle from '../../types/AbstractPuzzle';
+import * as fs from 'fs';
+import readFile from '../../utils/readFile';
 
 export default class ConcretePuzzle extends Puzzle {
-  public solveFirst(): string {
-    // WRITE SOLUTION FOR TEST 1
-    return 'day 1 solution 1';
-  }
+	private file = fs.readFileSync('src/days/1/input.txt').toString().split('\n');
 
-  public getFirstExpectedResult(): string {
-    // RETURN EXPECTED SOLUTION FOR TEST 1;
-    return 'day 1 solution 1';
-  }
+	public solveFirst(): string {
+		let groupTotal = 0;
+		let highestAmount = 0;
+		for (const i in this.file) {
+			if (this.file[i] != '') {
+				groupTotal += parseInt(this.file[i]);
+			} else {
+				if (groupTotal >= highestAmount) {
+					highestAmount = groupTotal;
+				}
+				groupTotal = 0;
+			}
+		}
+		return highestAmount.toString();
+	}
 
-  public solveSecond(): string {
-    // WRITE SOLUTION FOR TEST 2
-    return 'day 1 solution 2';
-  }
+	public getFirstExpectedResult(): string {
+		// RETURN EXPECTED SOLUTION FOR TEST 1;
+		return 'day 1 solution 1';
+	}
 
-  public getSecondExpectedResult(): string {
-    // RETURN EXPECTED SOLUTION FOR TEST 2;
-    return 'day 1 solution 2';
-  }
+	public solveSecond(): string {
+		let highestAmounts = [0, 0, 0];
+		let groupTotal = 0;
+
+		for (const i in this.file) {
+			if (this.file[i] != '') {
+				groupTotal += parseInt(this.file[i]);
+			} else {
+				highestAmounts = highestAmounts.sort((a, b) => a - b);
+				if (
+					highestAmounts.some((value) => {
+						return value < groupTotal;
+					})
+				) {
+					highestAmounts.shift();
+					highestAmounts.push(groupTotal);
+				}
+				groupTotal = 0;
+			}
+		}
+		return `Total calories: ${
+			highestAmounts[0] + highestAmounts[1] + highestAmounts[2]
+		}`;
+	}
+
+	public getSecondExpectedResult(): string {
+		// RETURN EXPECTED SOLUTION FOR TEST 2;
+		return 'day 1 solution 2';
+	}
 }
